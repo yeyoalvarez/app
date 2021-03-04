7ee172597537ead545c9115240781a1e38a81b9a
yeyoalvarez <gregorgg23@gmail.com>
2021-03-04 19:29:12 -0300

    //login inicio

--- app/settings.py
+++ app/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'widget_tweaks',
     #apps
-    'core.erp'
+    'core.erp',
+    'core.login',
 
 ]
 
@@ -122,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/erp/category/list/'
--- app/urls.py
+++ app/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core.login.views import LoginFormView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginFormView.as_view() ),
 ]
+
